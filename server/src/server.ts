49081bb2c8d1ec19497bdad1d71f951e07f3c786
IngridCBarbosa <ingridcarolinelima@hotmail.com>
2020-06-04 11:09:00 -0300

    add application functionalities

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,4 +1,5 @@
 import express, { response } from 'express';
+import path from 'path' // quando lidamos com caminhos
 import routes from './routes'
 
 
@@ -26,5 +27,7 @@ import routes from './routes'
 const app = express();
 app.use(express.json());
 app.use(routes);
+
+app.use('/uploads', express.static(path.resolve(__dirname,'..','uploads')))
 
 app.listen(3333);
